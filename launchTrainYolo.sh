a419a1aab8ce776eefe76104e2b6a539e5d40b65
Miguel Lima <up202108659@up.pt>
2026-04-20 22:26:18 +0100

    n config

--- launchTrainYolo.sh
+++ launchTrainYolo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min32gb   # Reserved partition
-#SBATCH --qos=gpu_min32gb
+#SBATCH --partition=gpu_min11gb   # Reserved partition
+#SBATCH --qos=gpu_min11gb
 #SBATCH --job-name=Yolo26Train_Enhanced
 #SBATCH --output=slurm_%x.%j.out
 #SBATCH --error=slurm_%x.%j.err
